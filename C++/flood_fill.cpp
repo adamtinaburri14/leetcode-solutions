e11182842c03c99097fe2e6762e79278567782a5
Adam Tinaburri <adamtinaburri14@gmail.com>
2025-11-27 23:17:48 -0800

    flood_fill.cpp completed

--- C++/flood_fill.cpp
+++ C++/flood_fill.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <deque>
+#include <queue>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> to_check = {{sr,sc}};
+        queue<vector<int>> to_check;
         int original_color = image[sr][sc];
-        while (to_check.size() > 0) {
-
-            if (image[to_check.front()[0]][to_check.front()[1]] == original_color){
-                cout << "success" << endl;
-                to_check.pop_front();
+        if (color != original_color) {
+                to_check.push({sr,sc});
             }
+        while (!to_check.empty()) {
+            int cr = to_check.front()[0];
+            int cc = to_check.front()[1];
+            if (image[cr][cc] == original_color){
+                image[cr][cc] = color;
+                // check up
+                if (cr > 0) {
+                    if (image[cr-1][cc] == original_color) {
+                        to_check.push({cr-1, cc});
+                    }
+                }
+                // check down
+                if (cr < image.size()-1) {
+                    if (image[cr+1][cc] == original_color) {
+                        to_check.push({cr+1, cc});
+                    }
+                }
+                // check left
+                if (cc > 0) {
+                    if (image[cr][cc-1] == original_color) {
+                        to_check.push({cr, cc-1});
+                    }
+                }
+                // check right
+                if (cc < image[0].size()-1){
+                    if (image[cr][cc+1] == original_color) {
+                        to_check.push({cr, cc+1});
+                    }
+                }
+            }
+            to_check.pop();
         }
         return image;
     }
@@ -21,12 +49,17 @@ public:
 
 int main() {
     Solution solution;
-
-    vector<vector<int>> image = {{1,1,1},{1,1,0},{1,0,1}};
-    int sr = 1;
-    int sc = 1;
-    int color = 2;
+    // vector<vector<int>> image = {{1,1,1},{1,1,0},{1,0,1}};
+    vector<vector<int>> image = {{0,0,0},{0,0,0}};
+    int sr = 0;
+    int sc = 0;
+    int color = 0;
     solution.floodFill(image, sr, sc, color);
-
+    for (const vector<int>& x : image) {
+        for (int val : x) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
